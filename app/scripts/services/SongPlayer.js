8533494199e88dbd746ffe8fc348a607d043401c
LeoQ <leoq91@gmail.com>
2016-12-01 10:53:34 -0800

    checkpoint-7-complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,26 +3,44 @@
         var SongPlayer = {};
 
         var currentSong = null;
+ /**
+ * @desc Buzz object audio file
+ * @type {Object}
+ */
      	var currentBuzzObject = null;
+ /**
+ * @function setSong
+ * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+ * @param {Object} song
+ */
+	 	var setSong = function(song) {
+		    if (currentBuzzObject) {
+		        currentBuzzObject.stop();
+		        currentSong.playing = null;
+		    }
+		 
+		    currentBuzzObject = new buzz.sound(song.audioUrl, {
+		        formats: ['mp3'],
+		        preload: true
+		    });
+		 
+		    currentSong = song;
+		};
         SongPlayer.play = function(song) {
         	if (currentSong !== song) {
-             	if (currentBuzzObject) {
-                 	currentBuzzObject.stop();
-             	}
-
-            	currentBuzzObject = new buzz.sound(song.audioUrl, {
-               		formats: ['mp3'],
-               		preload: true
-           		});
-
- 		   		currentSong = song;
-           		currentBuzzObject.play();   
+             	setSong(song);
+             	currentBuzzObject.play();
+         		song.playing = true;
            	} else if (currentSong === song) {
          		if (currentBuzzObject.isPaused()) {
              	currentBuzzObject.play();
          		}
      		}
         };
+        SongPlayer.pause = function(song) {
+		    currentBuzzObject.pause();
+		    song.playing = false;
+		};
 
         return SongPlayer;
      }
--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -4,25 +4,35 @@
 
         var currentSong = null;
      	var currentBuzzObject = null;
+
+	 	var setSong = function(song) {
+		    if (currentBuzzObject) {
+		        currentBuzzObject.stop();
+		        currentSong.playing = null;
+		    }
+		 
+		    currentBuzzObject = new buzz.sound(song.audioUrl, {
+		        formats: ['mp3'],
+		        preload: true
+		    });
+		 
+		    currentSong = song;
+		};
         SongPlayer.play = function(song) {
         	if (currentSong !== song) {
-             	if (currentBuzzObject) {
-                 	currentBuzzObject.stop();
-             	}
-
-            	currentBuzzObject = new buzz.sound(song.audioUrl, {
-               		formats: ['mp3'],
-               		preload: true
-           		});
-
- 		   		currentSong = song;
-           		currentBuzzObject.play();   
+             	setSong(song);
+             	currentBuzzObject.play();
+         		song.playing = true;
            	} else if (currentSong === song) {
          		if (currentBuzzObject.isPaused()) {
              	currentBuzzObject.play();
          		}
      		}
         };
+        SongPlayer.pause = function(song) {
+		    currentBuzzObject.pause();
+		    song.playing = false;
+		};
 
         return SongPlayer;
      }
